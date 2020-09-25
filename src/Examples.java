2c44cb56556731a8b95b458fa26f8ea89a3fe217
ZaneDario <dariohuertacontact@gmail.com>
2020-09-25 13:49:00 +0200

    Perceptron working most of the times.

--- src/Examples.java
+++ src/Examples.java
@@ -1,4 +1,7 @@
 public class Examples {
+//    public float[][] input = {{0,0},{0,1},{1,0},{1,1}};
+//    public float[] output = {0,0,0,1};
+
     public float[][] input = {{0,0},{0,1},{1,0},{1,1}};
-    public float[] output = {0,0,0,1};
+    public float[] output = {1,0,0,1};
 }
